7f478425d980b59806bdb94db185693055f6d816
Zebulon Rouse-Frantzich <zeb.frantzich@opscompass.com>
2026-05-27 11:19:01 -0500

    Updates to fix nvim-treesitter

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -1,32 +1,90 @@
 -- Highlight, edit, and navigate code
 local settings = require 'config.settings'
 
+local install_dir = vim.fn.stdpath 'data' .. '/site'
+local missing_parser_notified = {}
+local ignored_filetypes = {
+  oil = true,
+}
+
+local function maybe_notify_missing_parser(ft, lang)
+  if ft == '' or missing_parser_notified[lang] then
+    return
+  end
+
+  missing_parser_notified[lang] = true
+
+  vim.schedule(function()
+    vim.notify(
+      string.format(
+        "No Tree-sitter parser for '%s' (filetype '%s'). Run :TSInstall! %s. Use :TSLang to show filetype and Tree-sitter language again.",
+        lang,
+        ft,
+        lang
+      ),
+      vim.log.levels.INFO,
+      { title = 'treesitter' }
+    )
+  end)
+end
+
+local function maybe_set_indentexpr(buf, lang)
+  local ok, query = pcall(vim.treesitter.query.get, lang, 'indents')
+  if ok and query then
+    vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end
+end
+
 return {
   'nvim-treesitter/nvim-treesitter',
   enabled = settings.is_enabled('treesitter', 'treesitter'),
   lazy = false,
   build = ':TSUpdate',
-  config = function()
+  opts = {
+    install_dir = install_dir,
+  },
+  config = function(_, opts)
     local ts = require('nvim-treesitter')
 
-    ts.install({
-      'bash', 'c', 'diff', 'html', 'lua', 'luadoc',
-      'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc'
-    }):wait(30000)
+    ts.setup(opts)
+
+    vim.api.nvim_create_user_command('TSLang', function()
+      local ft = vim.bo.filetype
+      local lang = vim.treesitter.language.get_lang(ft) or ft
+
+      vim.notify(string.format("filetype=%s treesitter=%s", ft, lang), vim.log.levels.INFO, {
+        title = 'treesitter',
+      })
+    end, {
+      desc = 'Show current buffer filetype and Tree-sitter language',
+    })
 
     vim.api.nvim_create_autocmd('FileType', {
       group = vim.api.nvim_create_augroup('treesitter-setup', { clear = true }),
+      pattern = '*',
       callback = function(args)
-        local ft = vim.bo[args.buf].filetype
+        local buf = args.buf
 
-        pcall(vim.treesitter.start, args.buf)
-
-        if ft == 'ruby' then
-          vim.bo[args.buf].syntax = 'on'
+        if vim.bo[buf].buftype ~= '' then
+          return
         end
 
-        if ft ~= 'ruby' then
-          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        local ft = vim.bo[buf].filetype
+        if ft == '' or ignored_filetypes[ft] then
+          return
+        end
+
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+        local has_parser = vim.treesitter.language.add(lang)
+
+        if not has_parser then
+          maybe_notify_missing_parser(ft, lang)
+          return
+        end
+
+        local ok = pcall(vim.treesitter.start, buf, lang)
+        if ok then
+          maybe_set_indentexpr(buf, lang)
         end
       end,
     })
